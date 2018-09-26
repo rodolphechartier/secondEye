9e61127eb753b975eb951093cccdb1bd12b0e81a
Rodolphe Chartier <rodolphe.chartier@gmail.com>
2018-09-26 17:03:02 +0200

    visionController::get_landscape() function updated

--- API/api/controllers/visionController.js
+++ API/api/controllers/visionController.js
@@ -8,7 +8,40 @@ exports.test_vision = function(req, res) {
 };
 
 exports.get_landscape = function(req, res) {
-  res.json({ message: 'Test Vision OK' });
+    const params = {
+        'visualFeatures': 'Categories,Description,Color',
+    };
+    const sourceImageUrl = req.body.url;
+
+    const options = {
+        uri: vision_api_url + '/analyze',
+        qs: params,
+        body: '{"url": ' + '"' + sourceImageUrl + '"}',
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key' : vision_api_key
+        }
+    };
+
+    request.post(options, (error, response, body) => {
+        if (error) {
+            res.send(error);
+        }
+
+        let data = JSON.parse(body);
+        var response = '{"landscapes":[';
+        for (var i = 0; i < data["description"]["captions"].length; ++i) {
+            var text = data["description"]["captions"][i]["text"];
+
+            if (i)
+                response += ','
+            response += '{"message": ' + text '}';
+        }
+
+        response += ']}'
+        response = JSON.parse(response);
+        res.json(response);
+    });
 };
 
 exports.get_outdoors = function(req, res) {
