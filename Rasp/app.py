5f48d28fc958ded28cf6624a793e39bb7b542e66
pi <pi@raspberrypi.com>
2019-05-14 12:14:09 +0200

    list devices in an array

--- Rasp/app.py
+++ Rasp/app.py
@@ -1,11 +1,33 @@
+#!/usr/bin/python
+
 import os
 
 from evdev import InputDevice, categorize, ecodes
 
-# /dev/input/event0 => mouse
-# /dev/input/event1 => keyboard
-# /dev/input/event2 => broken keyboard 
-# /dev/input/event3 => middle headset button
+def list_devices():
+        file = open('/proc/bus/input/devices', 'r')
+        devices = []
+        buffer = ''
+
+        for line in file:
+                line = line.rstrip()
+                if len(line) > 0:
+                        buffer += line + "\n"
+                elif len(buffer) > 0:
+                        devices.append(buffer)
+                        buffer = ''
+
+        if len(buffer) > 0:
+                devices.append(buffer)
+                buffer = ''
+
+        file.close()
+
+        i = 0
+        while i < len(devices):
+                print('device n.{0}'.format(i))
+                print(devices[i])
+                i += 1
 
 def app():
 	try:
@@ -18,4 +40,4 @@ def app():
 			print(event)
 			os.system('raspistill -t 2 -o photo.jpg')
 
-app()
+list_devices()
